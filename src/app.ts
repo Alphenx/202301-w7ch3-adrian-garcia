036a899965f2a9100f5dab896607e2518fb0bd9d
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-03-02 21:22:14 +0100

    Add app and test

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+import apiRouter from './api-router';
+import cors from 'cors';
+
+const app = express();
+app.get('/', (_req, res) => {
+  res.json('Hello world');
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', apiRouter);
+
+export default app;
